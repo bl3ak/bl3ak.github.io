875b060bc1db70f708a603ac47bbe0abf4b19e9c
bl3ak <bleakscript3@gmail.com>
2022-01-18 17:28:26 -0600

    footer

--- cmp/mi-footer.js
+++ cmp/mi-footer.js
@@ -1,7 +1,7 @@
 class MiFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = /* html */
-            `Copyright &copy; BL3AK 2021`;
+            `Copyright &copy; BL3AK 2022`;
     }
 }
 customElements.define("mi-footer", MiFooter);
